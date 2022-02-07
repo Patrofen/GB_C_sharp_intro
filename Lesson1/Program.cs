bbd4bd7eacdddef3a3737ea862f54fc72da0ccb9
Evgeniy <aee1983@gmail.com>
2022-02-07 14:11:10 +0300

    ДЗ к Уроку №1

---
+++ Lesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя пользователя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет {name}, сегодня {DateTime.Now:D}");
+        }
+    }
+}
